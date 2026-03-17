07c9cc6624eba875425a0a9f4ef33aa54364332d
PassoLargoBR <145858924+mrdogecoinbr@users.noreply.github.com>
2026-03-17 08:05:54 -0300

    Update paidservicetip.js

--- paidservicetip.js
+++ paidservicetip.js
@@ -1298,7 +1298,7 @@ ${gameInfo}
                     </div>
                     <div class="orion-input-group">
                         <label>DIVIDER</label>
-                       <input id="p-div" class="orion-input" value="500" placeholder="0 = manual basebet">
+                       <input id="p-div" class="orion-input" value="600" placeholder="0 = manual basebet">
                     </div>
                 </div>
             </div>
